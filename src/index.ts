6b5dc845aac9e9c956fc1a7dbdb4007026416629
Ihor <igornepipenko@gmail.com>
2019-12-21 14:10:19 +0200

    Feature: start with type system [ref:no-ref]

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,56 @@
-import { speakSloth } from './file';
-import { speakSloth1 } from './file1';
-import { speakSloth2 } from './file2';
-import { speakSloth3 } from './file3';
-import { speakSloth4 } from './file4';
+// interface IUserAccount {
+//     firstName: string;
+//     age: number;
+// }
+//
+// let person = IUserAccount;
 
-speakSloth(['Hi', 'All', 'TS', 'is', 'Awesome']);
-speakSloth1(['Hi', 'All', 'TS', 'is', 'Awesome']);
-speakSloth2(['Hi', 'All', 'TS', 'is', 'Awesome']);
-speakSloth3(['Hi', 'All', 'TS', 'is', 'Awesome']);
-speakSloth4(['Hi', 'All', 'TS', 'is', 'Awesome']);
+// let userAccount = {
+//     age: 33,
+//     firstName: 'Ihor',
+// }
+// const {firstName, ...userAccountWithOutFirstName} = userAccount;
+//
+// let person: typeof userAccountWithOutFirstName;
+//
+// person = {
+//     age: 33
+// }
+
+/*class Acc {
+    public age = 33;
+    public firstName = 'Ihor';
+}
+
+let acc: Acc = new Acc();*/
+
+let isDone: boolean = true;
+
+let str: string = 'Ts is awesome';
+str = `${str} === ${isDone}`;
+
+let num = 1;
+num = 0xf00d;
+num = 0b1010;
+num = NaN;
+num.toFixed();
+
+let bigInt: bigint = 4n;
+
+let nill: null = null;
+let und: undefined = undefined;
+
+const key1: symbol = Symbol('key1');
+const key2: unique symbol = Symbol('key2');
+
+let obj = {
+    [key1]: 'Ts is awesome',
+};
+
+let value = obj[key1];
+//
+// function onlyKey1(v: typeof key1) {
+//
+// }
+//
+// onlyKey1(key2);
