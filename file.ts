22d9e2ba797abe04de828cdf1087867c7bf0a77c
Ihor <igornepipenko@gmail.com>
2019-12-21 11:51:31 +0200

    Feature: remove errors in ts [ref:no-ref]

--- file.ts
+++ file.ts
@@ -1,28 +1,30 @@
-"use strict";
 Object.defineProperty(window, "MySweetApp", { value: "v1.0.0", writable: true });
+
 function deliveryMethod() {
     return "overnight";
 }
+
 function shipWeight() {
-    var el = document.getElementById('weight');
-    if (el === null) {
+    const el:  HTMLElement | null = document.getElementById('weight');
+    if(el === null){
         return 0;
     }
     return parseInt(el.innerHTML);
 }
-function sendUpdates(emailAddr) {
-    function sendEmail(addr) {
-        console.log("Shipping to " + addr + " via " + (deliveryMethod() || "standard") + " delivery");
+
+function sendUpdates(emailAddr: string | string[]) {
+    function sendEmail(addr: string) {
+        console.log(`Shipping to ${addr} via ${deliveryMethod() || "standard"} delivery`);
+
         if (shipWeight() > 100) {
             console.log("WARNING: Oversize package");
         }
     }
     if (Array.isArray(emailAddr)) {
-        emailAddr.forEach(function (val) {
+        emailAddr.forEach((val) => {
             sendEmail(val.trim());
         });
-    }
-    else {
+    } else {
         sendEmail(emailAddr.trim());
     }
 }
