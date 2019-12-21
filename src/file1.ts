ffea174047fb1bd0ebd8863d6c9f08cb07d997c0
Ihor <igornepipenko@gmail.com>
2019-12-21 13:33:09 +0200

    Feature: webpack [ref:no-ref]

---
+++ src/file1.ts
@@ -0,0 +1,21 @@
+function sleep(ms: number) {
+    return new Promise((res) => {
+        setTimeout(res, ms);
+    });
+}
+
+async function* getItemsReallySlowly<T>(items: Iterable<T>): AsyncIterableIterator<T> {
+    for (const item of items) {
+        await sleep(1000);
+        yield item;
+    }
+}
+
+export async function speakSloth1(items: string[]) {
+    for await (const item of getItemsReallySlowly(items)) {
+        console.log(item);
+    }
+}
+
+
+
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { speakSloth } from './file';
+import { speakSloth1 } from './file1';
+import { speakSloth2 } from './file2';
+import { speakSloth3 } from './file3';
+import { speakSloth4 } from './file4';
+
+speakSloth(['Hi', 'All', 'TS', 'is', 'Awesome']);
+speakSloth1(['Hi', 'All', 'TS', 'is', 'Awesome']);
+speakSloth2(['Hi', 'All', 'TS', 'is', 'Awesome']);
+speakSloth3(['Hi', 'All', 'TS', 'is', 'Awesome']);
+speakSloth4(['Hi', 'All', 'TS', 'is', 'Awesome']);
