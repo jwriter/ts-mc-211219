d21c6aaa7ec130a606c46578865fd088707622d2
Ihor <igornepipenko@gmail.com>
2019-12-21 17:20:00 +0200

    Feature: mapped types [ref:no-ref]

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,33 @@
-// T extends U ? X : N
+// type NonReadonly<T> = {
+//     -readonly [P in keyof T]: T[P]
+// }
 //
-//  type nonUndefined<T> = T extends null | undefined ? never : T;
-//  type someType = string | null | undefined | number;
-// //
-//  const aa: NonNullable<someType> = true;
+// interface IAccount {
+//     readonly firstName?: string;
+//     readonly age: number;
+// }
 //
+// const acc: IAccount = {
+//     age: 33,
+//     firstName: 'Ihor',
+// }
 //
-// const arr: [() => number, number] = [() => 1, 1];
-// const arr1: [() => { firstName: string, age: number }, number] = [
-//     () => {
-//         return {firstName: 'Ihor', age: 33};
-//     },
-//     1,
-// ];
-//
-// type FirstType<T> =
-//     T extends [infer U, ...unknown[]]
-//         ? U extends (...args: unknown[]) => infer R ? R : never
-//         : never;
-//
-// const fn: FirstType<typeof arr> = 's';
-// const user: FirstType<typeof arr1> = {firstName: 'Ihor', age: 33};
-//
+// acc.firstName = 'Vald';
 //
 // let a: Exclude<any, any>
 
-function fn(_a: string, _b: boolean): number {
-    return 1;
-}   // => string | boolean | number
+type IPerson = {
+    readonly name: string;
+    age: number;
+    info: { male: boolean };
+    subjects: string[]
+}
 
-type NonFunction<T> = T extends Function ? never : T;
-type FunctionParamsAndReturn<T> = T extends (...args: infer Args) => infer R
-    ? keyof Args | R
-    : never;
+type RemoveByTypes<T, E>= {
+    [P in keyof T]: E extends T[P] ? never : P
+}[keyof T];
 
-const snb: FunctionParamsAndReturn<typeof fn> = {a: 1};
-
-let a: Exclude<any, any>
+const p: RemoveByTypes<IPerson, number > = 'age'; // 'name' | 'info' | 'subject'
+const userUpdate: Partial<IPerson> = {
+    age: 22;
+}
